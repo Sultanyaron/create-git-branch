50ccfdb63b5a933b0b67ea18aa141f3d989c94cd
sultanyaron <sultanyaron@gmail.com>
2023-08-30 17:58:58 +0300

    test1-add2

--- scripts/cloud-ui-modules-deploy.js
+++ scripts/cloud-ui-modules-deploy.js
@@ -1,2 +1,24 @@
 
 
+// import { spawnSync as spawn } from "child_process";
+
+const { CHANGED_FILES } = env
+
+// export const spawnSync = (command, args, options = {}, alwaysThrow) => {
+//   const ret = spawn(command, args, {
+//     stdio: "inherit",
+//     shell: true,
+//     ...options,
+//   });
+//   if (ret.error) {
+//     throw ret.error;
+//   } else if (ret.status !== 0) {
+//     if (alwaysThrow) {
+//       throw new Error();
+//     } else {
+//       process.exit(ret.status);
+//     }
+//   }
+// };
+
+console.log(CHANGED_FILES)
