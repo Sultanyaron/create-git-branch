6b661c1f578ab60032d252cf895a2ff5c05b3125
Yaron Sultan <sultanyaron@gmail.com>
2023-08-31 11:04:14 +0300

    test11-add1 (#22)

--- scripts/cloud-ui-modules-deploy.js
+++ scripts/cloud-ui-modules-deploy.js
@@ -1,49 +1,52 @@
-
-
-// import { spawnSync as spawn } from "child_process";
+import { spawnSync as spawn } from "child_process";
 
 const { CHANGED_FILES } = process.env;
+const AWS_S3_PROD_BUCKET = "s3://cloud-ui-modules-prod";
 
-// export const spawnSync = (command, args, options = {}, alwaysThrow) => {
-//   const ret = spawn(command, args, {
-//     stdio: "inherit",
-//     shell: true,
-//     ...options,
-//   });
-//   if (ret.error) {
-//     throw ret.error;
-//   } else if (ret.status !== 0) {
-//     if (alwaysThrow) {
-//       throw new Error();
-//     } else {
-//       process.exit(ret.status);
-//     }
-//   }
-// };
-console.log(CHANGED_FILES)
-
-const pathSegments = CHANGED_FILES.split(',');
-
-const result = pathSegments.map(path => {
-  const parts = path.split('/');
-  const moduleName = parts[parts.length - 1].replace('.json', '');
-  
-  let env = '';
-  if (path.includes('sm-prod')) {
-    env = 'prod';
-  } else if (path.includes('sm-staging')) {
-    env = 'staging';
+export const spawnSync = (command, args) => {
+  const ret = spawn(command, args, {
+    stdio: "inherit",
+    shell: true,
+  });
+  if (ret.error) {
+    throw ret.error;
+  } else if (ret.status !== 0) {
+    process.exit(ret.status);
   }
-  
+};
+console.log(CHANGED_FILES);
+
+const pathSegments = CHANGED_FILES.split(",");
+
+const mfesToUpdate = pathSegments.map((path) => {
+  const parts = path.split("/");
+  const moduleName = parts[parts.length - 1].replace(".json", "");
+
+  let env = "";
+  if (path.includes("sm-prod")) {
+    env = "prod";
+  } else if (path.includes("sm-staging")) {
+    env = "staging";
+  }
+
   const version = require(`../${path}`).version; // Assuming the version is a key inside the JSON file
-  
+
   return {
     moduleName,
     env,
-    version
+    version,
   };
 });
 
-console.log(result);
+for (const mfeToUpdate of mfesToUpdate) {
+  const { moduleName, version, env } = mfeToUpdate;
+  console.log(`Deploying ${moduleName}-${version} to ${env}`);
+  spawnSync(
+    "aws",
+    `s3 sync ${AWS_S3_PROD_BUCKET}/builds/modules/${moduleName}/${version} ${AWS_S3_PROD_BUCKET}/${env}/modules/${moduleName}`.split(
+      " "
+    )
+  );
+}
 
-
+console.log(mfesToUpdate);
