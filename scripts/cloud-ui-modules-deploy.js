d2625e93d538113b3d02d6c1824d25534e50ebd3
sultanyaron <sultanyaron@gmail.com>
2023-08-31 11:43:55 +0300

    test16-add1

--- scripts/cloud-ui-modules-deploy.js
+++ scripts/cloud-ui-modules-deploy.js
@@ -1,4 +1,4 @@
-const { spawnSync: spawn } = require("child_process");
+const { spawnSync } = require("child_process");
 
 const { CHANGED_FILES } = process.env;
 const AWS_S3_PROD_BUCKET = "s3://cloud-ui-modules-prod";
@@ -18,7 +18,7 @@ console.log(CHANGED_FILES);
 
 const pathSegments = CHANGED_FILES.split(",");
 
-const mfesToUpdate = pathSegments.map((path) => {
+const deployResults = pathSegments.map((path) => {
   const parts = path.split("/");
   const moduleName = parts[parts.length - 1].replace(".json", "");
 
@@ -29,26 +29,39 @@ const mfesToUpdate = pathSegments.map((path) => {
     env = "staging";
   }
 
-  const version = require(`../${path}`).version; // Assuming the version is a key inside the JSON file
+  const version = require(`../${path}`).version;
+
+  console.log(`Deploying ${moduleName}-${version} to ${env}`);
+  const childProcess = spawnSync(
+    "aws",
+    `s3 sync ${AWS_S3_PROD_BUCKET}/builds/modules/${moduleName}/${version} ${AWS_S3_PROD_BUCKET}/${env}/modules/${moduleName}`.split(
+      " "
+    ),
+    {
+      stdio: "inherit",
+      shell: true,
+    }
+  );
+
+  if (!childProcess.error) {
+    console.log(`✅ Successfully deployed ${moduleName}-${version} to ${env}`);
+  } else {
+    console.log(`❌ Failed to deploy ${moduleName}-${version} to ${env}`);
+  }
 
   return {
     moduleName,
     env,
     version,
+    error: childProcess.error,
   };
 });
 
-console.log(mfesToUpdate);
+console.log(deployResults);
 
-for (const mfeToUpdate of mfesToUpdate) {
-  const { moduleName, version, env } = mfeToUpdate;
-  console.log(`Deploying ${moduleName}-${version} to ${env}`);
-  spawnSync(
-    "aws",
-    `s3 sync ${AWS_S3_PROD_BUCKET}/builds/modules/${moduleName}/${version} ${AWS_S3_PROD_BUCKET}/${env}/modules/${moduleName}`.split(
-      " "
-    )
-  );
+const hasFailedDeployments = deployResults.some((result) => result.error);
+
+if (hasFailedDeployments) {
+  console.log("❌ Failed to deploy some modules");
+  // throw new Error();
 }
-
-console.log(mfesToUpdate);
